f72e0bf6722379846bb4c5df64cff77358e6d947
neox5 <faustmannchr@gmail.com>
2021-06-06 19:02:11 +0200

    0.0.1

--- src/test/hello-world.spec.ts
+++ src/test/hello-world.spec.ts
@@ -2,6 +2,6 @@ import { helloWorld } from "../index";
 
 describe("Testing hello world", () => {
   test("return value", () => {
-    expect(helloWorld()).toBe("Hello world!")
-  })
-})
+    expect(helloWorld()).toBe("Hello world!");
+  });
+});
