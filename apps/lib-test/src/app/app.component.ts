e35183e385a8c63eade8f2f609036df6c7a1a014
neox5 <faustmannchr@gmail.com>
2021-10-19 13:56:33 +0200

    Adds lib-test application

---
+++ apps/lib-test/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "test-root",
+  template: ` <h1>ezzy-web-crypto test</h1> `,
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {}
