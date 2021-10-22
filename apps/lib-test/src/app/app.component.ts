be5d7c0bff7c95d303fdebf08488dcd8835a4675
neox5 <faustmannchr@gmail.com>
2021-10-22 22:35:53 +0200

    Adds layout with container

--- apps/lib-test/src/app/app.component.ts
+++ apps/lib-test/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from "@angular/core";
 @Component({
   selector: "test-root",
   template: `
-    <h1>ezzy-web-crypto test</h1>
-    <test-manager></test-manager>
+    <div class="container">
+      <h1>ezzy-web-crypto test</h1>
+      <test-manager></test-manager>
+    </div>
   `,
   styleUrls: ["./app.component.scss"],
 })
